1ae896b58c1131b1be88379f0bdb37c85ec6d712
Jandalslap <128538609+Jandalslap@users.noreply.github.com>
2023-08-17 14:11:10 +1200

    Order Form

--- js/open_core.js
+++ js/open_core.js
@@ -3,10 +3,3 @@
 
 /* Product Order Form */
 
-function beforeSubmit() {
-	var firstName = document.getElementById("Subject");
-	var lastName = document.getElementById("LastName");
-	var body = document.getElementById("body");
-	subject.value = firstName.value + ' ' + lastName.value;
-	body.value = body.value + ' ' + product.value;
-}
