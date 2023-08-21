ba2d5007cc49edf2256cbdf7253b3a60419b5fc9
Jandalslap <128538609+Jandalslap@users.noreply.github.com>
2023-08-21 15:00:01 +1200

    Forms adjustment
    First and Last name letters only

--- js/open_core.js
+++ js/open_core.js
@@ -1,7 +1,23 @@
 
 /* Javascript for Open Core Website */
 
+/* Allow only alphabet letters in form First Name fields */
+const FName = document.getElementById('FName');
 
+FName.addEventListener('input', function() {
+  const inputValue = FName.value;
+  const onlyLetters = inputValue.replace(/[^A-Za-z]/g, '');
+  FName.value = onlyLetters;
+});
+
+/* Allow only alphabet letters in form Last Name fields */
+const LName = document.getElementById('LName');
+
+LName.addEventListener('input', function() {
+  const inputValue = LName.value;
+  const onlyLetters = inputValue.replace(/[^A-Za-z]/g, '');
+  LName.value = onlyLetters;
+});
 
 
 
